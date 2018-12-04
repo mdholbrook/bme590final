88906df3b06e4169bb2f53922d8f50df81bda839
Matt <mdh54@duke.edu>
2018-12-04 10:22:54 -0500

    Added button callbacks for applying processing and downloading data

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -1,138 +1,65 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'main_window.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.3
-#
-# WARNING! All changes made in this file will be lost!
-
-from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from PyQt5 import QtWidgets
+from GUI.main_window_design import Ui_MainWindow
 
 
-class Ui_MainWindow(object):
+class MainWindow(QtWidgets.QMainWindow):
 
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(600, 300)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox.setGeometry(QtCore.QRect(0, 60, 301, 201))
-        self.groupBox.setObjectName("groupBox")
-        self.verticalLayoutWidget = QtWidgets.QWidget(self.groupBox)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 20, 171, 171))
-        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.radioButton = QtWidgets.QRadioButton(self.verticalLayoutWidget)
-        self.radioButton.setChecked(True)
-        self.radioButton.setObjectName("radioButton")
-        self.buttonGroup = QtWidgets.QButtonGroup(MainWindow)
-        self.buttonGroup.setObjectName("buttonGroup")
-        self.buttonGroup.addButton(self.radioButton)
-        self.verticalLayout.addWidget(self.radioButton)
-        self.radioButton_3 = QtWidgets.QRadioButton(self.verticalLayoutWidget)
-        self.radioButton_3.setObjectName("radioButton_3")
-        self.buttonGroup.addButton(self.radioButton_3)
-        self.verticalLayout.addWidget(self.radioButton_3)
-        self.radioButton_4 = QtWidgets.QRadioButton(self.verticalLayoutWidget)
-        self.radioButton_4.setObjectName("radioButton_4")
-        self.buttonGroup.addButton(self.radioButton_4)
-        self.verticalLayout.addWidget(self.radioButton_4)
-        self.radioButton_2 = QtWidgets.QRadioButton(self.verticalLayoutWidget)
-        self.radioButton_2.setObjectName("radioButton_2")
-        self.buttonGroup.addButton(self.radioButton_2)
-        self.verticalLayout.addWidget(self.radioButton_2)
-        self.radioButton_5 = QtWidgets.QRadioButton(self.verticalLayoutWidget)
-        self.radioButton_5.setObjectName("radioButton_5")
-        self.buttonGroup.addButton(self.radioButton_5)
-        self.verticalLayout.addWidget(self.radioButton_5)
-        self.pushButton = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton.setGeometry(QtCore.QRect(200, 170, 91, 23))
-        self.pushButton.setObjectName("pushButton")
-        self.toolButton = QtWidgets.QToolButton(self.centralwidget)
-        self.toolButton.setGeometry(QtCore.QRect(470, 20, 25, 19))
-        self.toolButton.setObjectName("toolButton")
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(130, 20, 331, 20))
-        self.lineEdit.setObjectName("lineEdit")
-        self.groupBox_2 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_2.setGeometry(QtCore.QRect(330, 60, 241, 80))
-        self.groupBox_2.setObjectName("groupBox_2")
-        self.commandLinkButton = QtWidgets.QCommandLinkButton(self.groupBox_2)
-        self.commandLinkButton.setGeometry(QtCore.QRect(20, 20, 185, 41))
-        self.commandLinkButton.setObjectName("commandLinkButton")
-        self.groupBox_3 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_3.setGeometry(QtCore.QRect(330, 150, 251, 111))
-        self.groupBox_3.setObjectName("groupBox_3")
-        self.verticalLayoutWidget_3 = QtWidgets.QWidget(self.groupBox_3)
-        self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(10, 20, 131, 80))
-        self.verticalLayoutWidget_3.setObjectName("verticalLayoutWidget_3")
-        self.verticalLayout_3 = \
-            QtWidgets.QVBoxLayout(self.verticalLayoutWidget_3)
-        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout_3.setObjectName("verticalLayout_3")
-        self.radioButton_12 = \
-            QtWidgets.QRadioButton(self.verticalLayoutWidget_3)
-        self.radioButton_12.setObjectName("radioButton_12")
-        self.verticalLayout_3.addWidget(self.radioButton_12)
-        self.radioButton_13 = \
-            QtWidgets.QRadioButton(self.verticalLayoutWidget_3)
-        self.radioButton_13.setObjectName("radioButton_13")
-        self.verticalLayout_3.addWidget(self.radioButton_13)
-        self.radioButton_11 = \
-            QtWidgets.QRadioButton(self.verticalLayoutWidget_3)
-        self.radioButton_11.setObjectName("radioButton_11")
-        self.verticalLayout_3.addWidget(self.radioButton_11)
-        self.pushButton_3 = QtWidgets.QPushButton(self.groupBox_3)
-        self.pushButton_3.setGeometry(QtCore.QRect(160, 80, 75, 23))
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(40, 20, 71, 20))
-        self.label.setObjectName("label")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 602, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+    def __init__(self):
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        super(MainWindow, self).__init__()
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.groupBox.setTitle(_translate("MainWindow", "Image Corrections"))
-        self.radioButton.setText(
-            _translate("MainWindow", "Histogram Equalization"))
-        self.radioButton_3.setText(
-            _translate("MainWindow", "Contrast Stretching"))
-        self.radioButton_4.setText(_translate("MainWindow", "Log Compression"))
-        self.radioButton_2.setText(_translate("MainWindow", "Reverse Video"))
-        self.radioButton_5.setText(_translate("MainWindow", "Median Filter"))
-        self.pushButton.setText(_translate("MainWindow", "Apply Processing"))
-        self.toolButton.setText(_translate("MainWindow", "..."))
-        self.groupBox_2.setTitle(_translate("MainWindow", "Show Images"))
-        self.commandLinkButton.setText(
-            _translate("MainWindow", "Launch Image Viewer"))
-        self.groupBox_3.setTitle(
-            _translate("MainWindow", "Download Processed Image"))
-        self.radioButton_12.setText(_translate("MainWindow", "JPEG"))
-        self.radioButton_13.setText(_translate("MainWindow", "PNG"))
-        self.radioButton_11.setText(_translate("MainWindow", "TIFF"))
-        self.pushButton_3.setText(_translate("MainWindow", "Download"))
-        self.label.setText(_translate("MainWindow", "Input images"))
+        # Set up main window
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Callbacks for putton presses
+        self.ui.pushButtonApply.clicked.connect(self.apply_clicked)
+        self.ui.pushButtonDonwload.clicked.connect(self.download_clicked)
+
+        # Set up program dictionary
+        self.df = {}
+
+    def apply_clicked(self):
+        """The "Apply Processing" button was clicked
+
+        Returns:
+
+        """
+
+        # Load files and process
+
+        # Get processing settings
+        self.df['hist'] = self.ui.radioButtonHist.isChecked()
+        self.df['cont'] = self.ui.radioButtonContrast.isChecked()
+        self.df['log'] = self.ui.radioButtonLog.isChecked()
+        self.df['rev'] = self.ui.radioButtonReverse.isChecked()
+        self.df['median'] = self.ui.radioButtonMedian.isChecked()
+
+        print(self.df)
+
+        # TODO: Add call to communictation function
+
+    def download_clicked(self):
+        """The "Download" button was clicked
+
+        Returns:
+
+        """
+
+        # Get save settings
+        self.df['JPEG'] = self.ui.radioButtonJPEG.isChecked()
+        self.df['PNG'] = self.ui.radioButtonPNG.isChecked()
+        self.df['TIFF'] = self.ui.radioButtonTIFF.isChecked()
+
+        print(self.df)
+
+        # TODO: Add call to communictation function
 
 
 if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+
+    app = QtWidgets.QApplication([])
+    application = MainWindow()
+    application.show()
+    sys.exit(app.exec())
