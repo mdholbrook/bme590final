facad3e7a9bebed1b5c0f5434ee2329cb525a53e
alanr <registera13@gmail.com>
2018-12-04 15:29:40 -0500

    Add Outline for image Testing

---
+++ ImageProcessing/test_procesing.py
@@ -0,0 +1,17 @@
+import pytest
+import pytest_pep8
+
+
+def test_histogram_eq():
+
+
+def test_contrast_stretching():
+
+
+def test_log_compression():
+
+
+def test_reverse_video():
+
+
+def gamma_correction():
