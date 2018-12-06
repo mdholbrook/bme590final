6eece1a0064b55af5426489c49b0e56534145c51
Matt Holbrook <mdh54@duke.edu>
2018-12-06 16:38:10 -0500

    Added a function with associated unit test to read zipped image files

--- ClientFunctions/read_files.py
+++ ClientFunctions/read_files.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+from PIL import Image
 from zipfile import ZipFile
 
 
@@ -34,3 +36,18 @@ def get_zip_names(zipfilename):
             zipfiles.append(file.filename)
 
     return zipfiles
+
+
+def load_zipped_image(zipfilename):
+
+    # Read each image and append in a list
+    img = []
+    with ZipFile(zipfilename) as archive:
+        for entry in archive.infolist():
+            with archive.open(entry) as file:
+                print(file)
+                tmp = Image.open(file)
+                img.append(np.array(tmp))
+
+    # Return the read images
+    return img
--- test_ClientFunctions/test_read_files.py
+++ test_ClientFunctions/test_read_files.py
@@ -15,6 +15,42 @@ def test_load_image(file, shape):
 
 def test_get_zip_names():
 
-    zipfile = 'TestImages/Ims.zip'
+    zipfile = 'TestImages/TestImages.zip'
+    names = ['circles.png',
+             'circlesBrightDark.png',
+             'coins.png', 'foosball.jpg',
+             'foosballraw.tiff',
+             'football.jpg',
+             'Lenna.png',
+             'tire.tif',
+             'yellowlily.jpg'
+             ]
 
-    get_zip_names(zipfile)
+    zipnames = get_zip_names(zipfile)
+
+    assert zipnames == names
+
+
+def test_load_zipped_image():
+
+    # Set up test case
+    filename = 'TestImages/TestImages.zip'
+    file_sizes = [(256, 256),
+                  (512, 512),
+                  (246, 300),
+                  (2336, 3504, 3),
+                  (2348, 3522),
+                  (256, 320, 3),
+                  (220, 220, 3),
+                  (205, 232),
+                  (1632, 1224, 3)]
+
+    # Test function
+    ims = load_zipped_image(filename)
+
+    # Assert that the correct number of images were found
+    assert len(ims) == len(file_sizes)
+
+    # Assert that the images shapes are correct
+    for i in range(len(ims)):
+        assert ims[i].shape == file_sizes[i]
