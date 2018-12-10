66cbc615583a85a5aca3276571c93a5bd51dd2e7
Matt Holbrook <mdh54@duke.edu>
2018-12-10 17:07:28 -0500

    Created a function to save a zip file of images

--- ClientFunctions/write_files.py
+++ ClientFunctions/write_files.py
@@ -20,32 +20,51 @@ def write_image(filename, im):
     cv2.imwrite(filename, im)
 
 
-def write_zip(filename, files, ims, fileformat, file_ext):
+def write_zip(filename, files, ims, fileformat):
+    """
+    This function makes an in-memory zip file of image files and writes them
+    to disk.
+
+    Args:
+        filename (str): name of the zip file which will be saved to disk
+        files (list of str): list of filenames generated for saving, one for
+            each image to be saved
+        ims (list of numpy array): a list of image data to be saved
+        fileformat (str): file encoding method to be used. Valid values are
+        "JPEG", "PNG", and "TIFF".
+
+    Returns:
+
+    """
 
     # Set up in-memory file
     memory_file = BytesIO()
 
-    # Make a zip file in memory_file
+    # Make a zip file inside of the in-memory file
     zf = ZipFile(memory_file, mode='w')
 
-    for z in range(len(files)):
+    # For each image
+    for i in range(len(files)):
 
-        # Get image
-        im = Image.fromarray(ims[z])
+        # Get a single image and create a PIL object
+        im = Image.fromarray(ims[i])
 
         # Set up a temporary image buffer
         tmp = BytesIO()
 
-        # Save image to temporoary buffer
-        im.save(memory_file, fileformat)
+        # Save image to a temporary buffer using an encoding defined by
+        # fileformat
+        im.save(tmp, fileformat)
 
-        # Generate new filename using filename and extension
-        base = os.path.basename(files[z])
-        file, _ = os.path.splitext(base)
-        file = file + file_ext
+        # Save image into the in-memory zip file
+        zf.write(files[i], tmp.getvalue())
 
+    # Close the in-memory zip file
+    zf.close()
 
-    zf.write(filename, memory_file.getvalue())
+    # Write zip file to disk as bytes encoding
+    with open(filename, 'wb') as f:
+        f.write(memory_file.getvalue())
 
 
 def gen_save_filename(file, file_ext):
